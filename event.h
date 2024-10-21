e24d8698b43425a73a894de57b7814fd8068c2bd
Enzo <enzo.t@lycee-de-beauvoir.education>
2024-10-21 12:14:40 +0200

    Code qui fonctionne (real git init)

---
+++ event.h
@@ -0,0 +1,10 @@
+// event.h
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <iostream>
+using namespace std;
+
+void event(string _name); // Déclaration d'une fonction
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,30 +1,44 @@
-/*****************************
-Hector HAMELIN, Enzo TSANGOUAEKA - 20/10/2024
-*****************************/
+/**************************************************************
+main.cpp ------ Hector HAMELIN, Enzo TSANGOUAEKA - 20/10/2024
+**************************************************************/
 
-#include <iostream> // Gestion des entrées, sorties
-#include <cstdlib> // Aléatoire
-#include "event.cpp"
+#include <iostream>  // Gestion des entrées, sorties
+#include <cstdlib>   // Aléatoire
+#include <string>    // Inclusion de string
+#include "event.h"   // Utilisation des guillemets pour inclure le fichier local
 
 using namespace std;
 
 // Routine du programme
 void loop();
 
+// Variable Global
+int nmbrOfSpecimen(10);
+
+bool IsAlive() {
+    if(nmbrOfSpecimen > 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 struct SPECIMEN {
     string name;
 };
 
 void loop() {
-    cout << "Hello World!" << endl;
-    while (true) {
+    while (IsAlive()) {
         event("a");
+        nmbrOfSpecimen--;
     }
+
 }
 
 
 int main() {
     srand(time(0));
-    std::cout << "Hello, World!" << std::endl;
+    loop();
     return 0;
 }
